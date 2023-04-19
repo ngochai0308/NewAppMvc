21ccdb895a93ff8bd71fa7c6c6150a52467aa348
NgocHai <gmNgocHai0308@gmail.com>
2023-04-19 09:39:04 +0700

    Contact

---
+++ Areas/Contact/Controllers/ContactController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AppMvc.Models.Contact;
+using NewAppMvc.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NewAppMvc.Areas.Contact.Controllers
+{
+    [Area("Contact")]
+    public class ContactController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContactController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contact/Contact
+        [HttpGet("/admin/contact")]
+        public async Task<IActionResult> Index()
+        {
+              return _context.Contacts != null ? 
+                          View(await _context.Contacts.ToListAsync()) :
+                          Problem("Entity set 'AppDbContext.Contacts'  is null.");
+        }
+
+        [HttpGet("/admin/contact/details/{id}")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contactModel = await _context.Contacts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contactModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactModel);
+        }
+
+        [HttpGet("/contact/")]
+        [AllowAnonymous]
+        public IActionResult SendContact()
+        {
+            return View();
+        }
+        [TempData]
+        public string StatusMessage { set; get; }
+        [HttpPost("/contact/")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SendContact([Bind("FullName,Email,Message,Phone")] ContactModel contactModel)
+        {
+            if (ModelState.IsValid)
+            {
+                contactModel.DateSent = DateTime.Now;
+                _context.Add(contactModel);
+                await _context.SaveChangesAsync();
+                StatusMessage = "Liên hệ của bạn đã được gửi";
+                return RedirectToAction("Index","Home");
+            }
+            return View(contactModel);
+        }
+
+
+        [HttpGet("/admin/contact/delete/{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contactModel = await _context.Contacts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contactModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactModel);
+        }
+
+        // POST: Contact/Contact/Delete/5
+        [HttpPost("/admin/contact/delete/{id}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Contacts == null)
+            {
+                return Problem("Entity set 'AppDbContext.Contacts'  is null.");
+            }
+            var contactModel = await _context.Contacts.FindAsync(id);
+            if (contactModel != null)
+            {
+                _context.Contacts.Remove(contactModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using AppMvc.Models.Contact;
+
+namespace NewAppMvc.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           /* foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }*/
+
+        }
+
+        public DbSet<ContactModel> Contacts { get; set; }
+        /*public DbSet<ContactModel> Contacts { get; set; }*/
+    }
+}
